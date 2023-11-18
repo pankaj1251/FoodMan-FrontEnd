eca36b4f2f7623d144dd2d5813bd2a3d3ecb4e9d
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-11-19 01:20:24 +0530

    updated the modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import NotLoggedIn from './components/beforeLogin/NotLoggedIn';
+import NotLoggedIn from "./components/beforeLogin/NotLoggedIn";
 import LoggedIn from "./components/beforeLogin/LoggedIn";
 import { authSliceActions } from "./store/auth-slice";
 
